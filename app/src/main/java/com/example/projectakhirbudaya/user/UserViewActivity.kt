399f1601da3e325eff89ee9b19ab3af3d142af1f
Samms206 <sh2069936@gmail.com>
2024-06-10 17:08:52 +0700

    updated

--- app/src/main/java/com/example/projectakhirbudaya/user/UserViewActivity.kt
+++ app/src/main/java/com/example/projectakhirbudaya/user/UserViewActivity.kt
@@ -5,9 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projectakhirbudaya.PopUpFragment
 import com.example.projectakhirbudaya.R
+import com.example.projectakhirbudaya.adapter.BudayaAdapterForUser
+import com.example.projectakhirbudaya.adapter.BudayaAdapterRoom
+import com.example.projectakhirbudaya.room.BudayaEntity
+import com.example.projectakhirbudaya.viewmodel.BudayaViewModel
+import com.example.projectakhirbudaya.viewmodel.BudayaViewModelFactory
 
 class UserViewActivity : AppCompatActivity() {
+
+    private lateinit var budayaViewModel: BudayaViewModel
+    private lateinit var budayaAdapter : BudayaAdapterForUser
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +30,25 @@ class UserViewActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val factory = BudayaViewModelFactory.getInstance(this)
+        budayaViewModel = ViewModelProvider(this, factory)[BudayaViewModel::class.java]
+        recyclerView = findViewById(R.id.rv_budayaUser)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        budayaViewModel.getAllBudaya().observe(this) { data ->
+            if (data != null) {
+                budayaAdapter = BudayaAdapterForUser(data, budayaViewModel) //ini
+                recyclerView.adapter = budayaAdapter
+
+//                budayaAdapter.setOnItemClickCallback(object :
+//                    BudayaAdapterRoom.OnItemClickCallback {
+//                    override fun onMoreClicked(data: BudayaEntity, position: Int) {
+//                        PopUpFragment(data, position).show(supportFragmentManager, PopUpFragment.TAG)
+//                    }
+//                })
+            }
         }
     }
 }
